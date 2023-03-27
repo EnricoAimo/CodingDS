e8e3fcfd1d7ae609a6a8c0a64198bb80c39e7560
DiegoSgammeglia <101456085+DiegoSgammeglia@users.noreply.github.com>
2023-03-27 10:07:45 +0200

    aggiunti test

---
+++ ingSoftwareWorkspace/IngSoftwareCode/src/test/CalculatorTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import src.Calculator;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		
+	    Calculator calculator = new Calculator();
+	    int result = calculator.add(2, 3);
+	    assertEquals(5, result);
+	}
+
+	@Test
+	public void testSubtract() {
+	    Calculator calculator = new Calculator();
+	    int result = calculator.subtract(5, 3);
+	    assertEquals(2, result);
+	}
+
+	@Test(expected = ArithmeticException.class)
+	public void testDivideByZero() {
+	    Calculator calculator = new Calculator();
+	    int result = calculator.divide(5, 0);
+	}
+
+}
